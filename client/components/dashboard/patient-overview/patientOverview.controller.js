27455a45181a801552da43215cdfb161e0645547
Singh, Satnam <v-singhs-kfo@ae.com>
2014-12-12 18:39:33 -0800

    implemented patient dashboard. All content is static right now

---
+++ client/components/dashboard/patient-overview/patientOverview.controller.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('gaheenApp').controller('PatientoverviewCtrl', function ($scope, $location, Auth) {
+	$scope.getCurrentUser = Auth.getCurrentUser;
+	$scope.isAdmin = Auth.isAdmin;
+	$scope.isCollapsed = true;
+	$scope.isLoggedIn = Auth.isLoggedIn;
+	$scope.menu = [
+		{
+			'title': 'Home',
+			'link': '/'
+		}
+	];
+	$scope.isActive = function (route) {
+		return route === $location.path();
+	};
+	$scope.logout = function () {
+		Auth.logout();
+		$location.path('/login');
+	};
+});
